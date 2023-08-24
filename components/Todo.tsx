c0835bc8d78a204b39bc54059adfbb1551cc34f8
Cooper Saye <coopersaye@gmail.com>
2023-08-24 09:42:22 -0700

    Implement todo priority

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,26 +1,50 @@
 import { TodoData } from '@/utils/types'
 import styles from '../styles/components/Todo.module.scss'
 import { getAuth } from 'firebase/auth'
-import { deleteDoc, doc, getFirestore } from 'firebase/firestore'
+import { deleteDoc, doc, getFirestore, updateDoc } from 'firebase/firestore'
+import { useState } from 'react'
 
-export default function Todo({ emoji, note, id }: TodoData) {
+export default function Todo(props: TodoData) {
+  const { emoji, note, id } = props
+
   const auth = getAuth()
   const db = getFirestore()
 
+  const [priority, setPriority] = useState(props.priority)
+
+  const { uid } = auth.currentUser ?? {}
+  if (!uid) {
+    throw new Error('Todo with no user')
+  }
+  const todoRef = doc(db, 'users', uid, 'todos', id)
+
   return (
-    <div className={styles.container}>
-      <p>{emoji} {note}</p>
-      <button onClick={() => deleteTodo()}>&times;</button>
+    <div
+      className={
+        priority ? `${styles.container} ${styles.priority}` : styles.container
+      }
+      onClick={() => togglePriority()}
+    >
+      <p>
+        {emoji} {note}
+      </p>
+      <button
+        onClick={(e) => {
+          e.stopPropagation()
+          deleteTodo()
+        }}
+      >
+        &times;
+      </button>
     </div>
   )
 
-  async function deleteTodo() {
-    const { uid } = auth.currentUser ?? {}
-    if (!uid) {
-      throw new Error('Deleting todo with no user')
-    }
+  async function togglePriority() {
+    setPriority(!priority)
+    await updateDoc(todoRef, { priority: !priority })
+  }
 
-    const todoRef = doc(db, 'users', uid, 'todos', id)
+  async function deleteTodo() {
     await deleteDoc(todoRef)
   }
 }
